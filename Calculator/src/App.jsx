9c926d2341a155a4a4666c27a782dd6ae4291035
NodeJS <kashids961@gmail.com>
2025-07-24 12:33:26 +0530

    App.jsx

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -13,22 +13,28 @@ class App extends React.Component {
   render() {
   return <>
    <div className="bdy">
-    <h1>Calculator</h1>
+   
   <div className="cont">
+     <h1>Calculator</h1>
       <input type="text" name='first' value={this.state.first} placeholder='Enter first number' className="form-control"
-       onChange={(e)=> this.setState({first:e.target.value})} /><br /><br />
+       onChange={(e)=> this.setState({first:e.target.value})} /><br />
       <input type="text" name='second' value={this.state.second} placeholder='Enter second number'
-       className="form-control"  onChange={(e)=> this.setState({second:e.target.value})} /><br /><br />
+       className="form-control"  onChange={(e)=> this.setState({second:e.target.value})} /><br />
       <input type="text" value={this.state.result} placeholder='Result'/><br /><br />
-     
-        <input type="button" value="add" 
+     <div className="btn">
+      <div className="btn1">
+        <input type="button" value="add" style={{backgroundColor:'ButtonHighlight'}}
       onClick={(e)=> this.setState({result:parseInt(this.state.first)+parseInt(this.state.second)})}/> <br />
       <input type="button" value="sub" 
       onClick={(e)=> this.setState({result:parseInt(this.state.first)-parseInt(this.state.second)})}/> <br />
+      </div>
+      <div className="btn2">
       <input type="button" value="mul" 
       onClick={(e)=> this.setState({result:parseInt(this.state.first)*parseInt(this.state.second)})}/> <br />
       <input type="button" value="div" 
       onClick={(e)=> this.setState({result:parseInt(this.state.first)/parseInt(this.state.second)})}/> <br />
+      </div>
+      </div>
       </div>
       </div>
     </>
